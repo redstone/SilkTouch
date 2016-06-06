c8980852ffa15dc738eb92a5ab4ffedd22c5866c
Mark Hughes <m@rkhugh.es>
2016-06-06 14:07:32 +1000

    Tidy up, adding log and warn methods, and removing deprecated code

--- src/main/java/net/redstoneore/silktouch/SilkTouch.java
+++ src/main/java/net/redstoneore/silktouch/SilkTouch.java
@@ -4,24 +4,30 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 import org.spongepowered.api.Sponge;
-import org.spongepowered.api.block.BlockType;
-import org.spongepowered.api.block.BlockTypes;
 import org.spongepowered.api.config.ConfigDir;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.game.state.GameInitializationEvent;
-import org.spongepowered.api.item.ItemType;
-import org.spongepowered.api.item.ItemTypes;
 import org.spongepowered.api.plugin.Plugin;
 import org.spongepowered.api.plugin.PluginContainer;
 
 import com.google.inject.Inject;
 
+import net.redstoneore.silktouch.store.Config;
+
 @Plugin(id = "net.redstoneore.silktouch", name = "SilkTouch")
 public class SilkTouch {
+	
+	// ----------------------------------------
+	// SINGLETON
+	// ----------------------------------------
 	
 	private static SilkTouch i;
 	public static SilkTouch get() { return i; }
 	public SilkTouch() { i = this; }
+	
+	// ----------------------------------------
+	// FIELDS
+	// ----------------------------------------
 	
 	@Inject
 	@ConfigDir(sharedRoot = false)
@@ -29,6 +35,10 @@ public class SilkTouch {
 	
 	@Inject
 	private PluginContainer plugin;
+	
+	// ----------------------------------------
+	// METHODS
+	// ----------------------------------------
 	
 	@Listener
 	public void enable(GameInitializationEvent event) {
@@ -44,18 +54,20 @@ public class SilkTouch {
 		Sponge.getEventManager().registerListeners(this, SilkTouchListener.get());
 	}
 	
+	public void log(String msg) {
+		this.getPlugin().getLogger().info(msg);
+	}
+	
+	public void warn(String msg) {
+		this.getPlugin().getLogger().warn(msg);
+	}
+	
 	public PluginContainer getPlugin() {
 		return this.plugin;
 	}
 	
 	public Path getConfigDirectory() {
 		return this.privateConfigDir;
-	}
-	
-	public ItemType findDropFor(BlockType type) {
-		if (type == BlockTypes.CAKE) return ItemTypes.CAKE;
-		
-		return null;
 	}
 	
 }
